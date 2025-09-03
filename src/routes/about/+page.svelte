34f06a4315b17e2ba986854ee2a12c32390e995f
Ben <bennrhodes@gmail.com>
2025-09-03 17:18:16 +1000

    Update +page.svelte

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -9,7 +9,7 @@
   <p>Hey, I'm Ben 👋</p>
   <p>I'm a full-stack developer who loves building things that actually make a difference. Right now, I'm focused on solidifying myself as a tech lead, leveling up my UI chops, and pushing forward into technical leadership while experimenting with new ideas and products.</p>
   <p>But I'm not just about code. I'm a DIY enthusiast currently transforming a shipping container into a home office/studio — lovingly nicknamed the "work womb." I get a huge kick out of strategy games like Magic: The Gathering, Hearthstone, and Marvel Snap (studying the meta and different playstyles is basically brain yoga for me).</p>
-  <p>Outside of work, you'll probably find me cooking (Mexican and Italian are my go-tos), hanging out with my cat Otis, dog Otis (yes, double Otis), and my partner Elly. Long term, I've got big dreams of moving somewhere more remote to live a little slower and create a little more.</p>
+  <p>Outside of work, you'll probably find me cooking (Mexican and Italian are my go-tos), hanging out with my partner Elly, cat Otis and dog Ace. Long term, I've got big dreams of moving somewhere more remote to live a little slower and create a little more.</p>
   <p>At the heart of everything, my values stay pretty simple:</p>
 
   <ul class="list-disc list-inside space-y-2 mb-4">
